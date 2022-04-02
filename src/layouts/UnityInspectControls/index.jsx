b405c648e2e477f797f860d946e2699ba315e01a
Joe Boylson <joeboylson@gmail.com>
2022-04-01 23:27:29 -0400

    updates 04-01-2022

--- src/layouts/UnityInspectControls/index.jsx
+++ src/layouts/UnityInspectControls/index.jsx
@@ -29,9 +29,9 @@ const UnityInspectControls = ({unityContext}) => {
     if (inspectImageParams?.length === 4) {
       const [ size, frame, position, url ] = inspectImageParams;
       return {
-        size,
-        frame, 
-        position,
+        size: size || 0,
+        frame: frame || 1, 
+        position: position || 0,
         url, 
         texture: null, 
         inspectType: "unity-inspect"
@@ -42,11 +42,11 @@ const UnityInspectControls = ({unityContext}) => {
       const _inspectType = inspectType === "unity-empty-inspect" ? "unity-small-inspect" : "unity-large-inspect"
       const [ position, texture, url ] = inspectImageParams;
       return {
-        size: null,
-        frame: null, 
-        position,
+        size: 0,
+        frame: 1, 
+        position: position || 0,
         url, 
-        texture,
+        texture: texture || 0,
         inspectType: _inspectType
       }
     }
@@ -59,7 +59,8 @@ const UnityInspectControls = ({unityContext}) => {
       inspectType: null
     }
 
-  }, [inspectImageRaw])
+  }, [inspectImageRaw]);
+
 
   const showFrameOptions = useCallback(() => setOption(options.frameOptions), [])
   const showSizeOptions = useCallback(() => setOption(options.sizeOptions), [])
@@ -77,6 +78,16 @@ const UnityInspectControls = ({unityContext}) => {
     const { path: url } = selectedContent
     const outputString = `${size},${frame},${position},http://localhost:5000${url}`
     unityContext.send("GameController", "ArtJson", outputString);
+    overrideInspectedImage(outputString)
+    clearUI()
+  }, [inspectedImage, unityContext, clearUI, overrideInspectedImage])
+
+  const handleObjContentSelect = useCallback((selectedContent) => {
+    const texture = 0;
+    const { position } = inspectedImage;
+    const { path: url } = selectedContent
+    const outputString = `${position},${texture},http://localhost:5000${url}`
+    unityContext.send("GameController", "ObjSJson", outputString);
     overrideInspectedImage(outputString)
     clearUI()
   }, [inspectedImage, unityContext, clearUI, overrideInspectedImage])
@@ -119,76 +130,101 @@ const UnityInspectControls = ({unityContext}) => {
   }, [unityContext, inspectedImage, overrideInspectedImage])
 
   const fileExists = useMemo(() => !isEmpty(inspectedImage?.url), [inspectedImage] );
+
+  const ImageInspectControls = () => {
+    switch (option) {
   
-  const showType = useMemo(() => {
-    const inspectType = inspectedImage?.inspectType;
-    switch(inspectType) {
-      case inspectTypes.unityInspect:
-        return "image";
-      case inspectTypes.loadLargeObject:
-        return "objects3d";
-      case inspectTypes.loadSmallObject:
-        return "objects3d";
-      default:
-        return;
+      case options.frameOptions:
+        return (
+          <div id="unity-inspect-controls">
+            <ClearUIButton/>
+            <button onClick={() => changeCurrentFrame(0)}>Brown</button>
+            <button onClick={() => changeCurrentFrame(1)}>White</button>
+            <button onClick={() => changeCurrentFrame(2)}>Black</button>
+          </div>
+        );
+    
+      case options.sizeOptions:
+        return (
+          <div id="unity-inspect-controls">
+            <ClearUIButton/>
+            <button onClick={() => changeCurrentSize(0)}>Square</button>
+            <button onClick={() => changeCurrentSize(1)}>Portrait</button>
+            <button onClick={() => changeCurrentSize(2)}>Landscape</button>
+          </div>
+        );
+    
+    
+        default:    
+          return (
+            <div id="unity-inspect-controls">
+    
+              { contentListIsVisible && 
+                <ContentListModal 
+                  showType={"image"}
+                  handleCancel={hideContentList}
+                  handleContentSelect={handleContentSelect}
+                />
+              }
+    
+              <button onClick={showContentList}>
+                {fileExists ? "Change" : "Add"} Image
+              </button>
+              
+              <button onClick={showSizeOptions} disabled={!fileExists}>
+                Change Image Size
+              </button>
+    
+              <button onClick={showFrameOptions} disabled={!fileExists}>
+                Change Image Frame
+              </button>
+              
+              <button onClick={handleRemoveContentAtPosition} disabled={!fileExists}>
+                Remove Image
+              </button>
+            </div>
+          );
     }
+  };
+
+  const ImageObjControls = () => {
+
+    return (
+      <div id="unity-inspect-controls">
+
+        { contentListIsVisible && 
+          <ContentListModal 
+            showType={"objects3d"}
+            handleCancel={hideContentList}
+            handleContentSelect={handleObjContentSelect}
+          />
+        }
+
+        <button onClick={showContentList}>
+          {fileExists ? "Change" : "Add"} Object
+        </button>
+        
+        <button onClick={handleRemoveContentAtPosition} disabled={!fileExists}>
+          Remove Object
+        </button>
+      </div>
+    );
+  };
+
+  console.log({inspectedImage})
+
+  const noObjectSelected = useMemo(() => {
+    return Object.values(inspectedImage).every(i => i === null);
   }, [inspectedImage])
 
-  if (paused) return null;
+  if (paused || noObjectSelected) return null;
 
-  switch (option) {
-
-    case options.frameOptions:
-      return (
-        <div id="unity-inspect-controls">
-          <ClearUIButton/>
-          <button onClick={() => changeCurrentFrame(0)}>Brown</button>
-          <button onClick={() => changeCurrentFrame(1)}>White</button>
-          <button onClick={() => changeCurrentFrame(2)}>Black</button>
-        </div>
-      );
-
-    case options.sizeOptions:
-      return (
-        <div id="unity-inspect-controls">
-          <ClearUIButton/>
-          <button onClick={() => changeCurrentSize(0)}>Square</button>
-          <button onClick={() => changeCurrentSize(1)}>Portrait</button>
-          <button onClick={() => changeCurrentSize(2)}>Landscape</button>
-        </div>
-      );
-  
-
-    default:
-      return (
-        fileExists && <div id="unity-inspect-controls">
-
-          { contentListIsVisible && 
-            <ContentListModal 
-              showType={showType}
-              handleCancel={hideContentList}
-              handleContentSelect={handleContentSelect}
-            />
-          }
-
-          <button onClick={showContentList}>
-            {fileExists ? "Change" : "Add"} Image
-          </button>
-          
-          <button onClick={showSizeOptions} disabled={!fileExists}>
-            Change Image Size
-          </button>
-
-          <button onClick={showFrameOptions} disabled={!fileExists}>
-            Change Image Frame
-          </button>
-          
-          <button onClick={handleRemoveContentAtPosition} disabled={!fileExists}>
-            Remove Image
-          </button>
-        </div>
-      );
+  if (inspectedImage?.inspectType === "unity-inspect") {
+    return <ImageInspectControls/>
+  } else {
+    return <ImageObjControls/>
   }
+
 };
 
 export default UnityInspectControls;
